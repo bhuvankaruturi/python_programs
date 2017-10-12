6efdcdfc6ad1fef9796a316c09f8735ed0b5c5f3
bhuvankaruturi <bhuvankaruturi123@gmail.com>
2017-10-12 20:08:12 +0530

    Added few more lines of code

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -2,6 +2,10 @@ print('Trail on exception handling')
 
 try:
     print('Execution starts inside try block')
+    name = input('What is your name: ')
+    print('Hello,',name)
+    age = input('What is your age: ')
+    age = int(age)
     print('hello'+10)
     import mars
 
@@ -10,6 +14,7 @@ except TypeError as t:
 except NameError as n:
     print('NameError triggered')
 except Exception as e:
+    print('***Some general exception occured***')
     print(str(e))
 
 print('Execution continues on')
